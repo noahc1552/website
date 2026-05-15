a455ef90e6f856afd3b6b28bcc95c306568eaf70
Noah C <259662408+noahc1552@users.noreply.github.com>
2026-05-14 22:41:32 -0400

    Updated projects

--- js/script.js
+++ js/script.js
@@ -41,5 +41,28 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
     }
+
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    document.querySelector('.prev').addEventListener('click', () => plusSlides(-1));
+    document.querySelector('.next').addEventListener('click', () => plusSlides(1));
+
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex-1].style.display = "block";
+    } 
 });
 
